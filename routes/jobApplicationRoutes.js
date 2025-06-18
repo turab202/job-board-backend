4576396aca57a4861f1a4a011a392228bda49de5
turab202 <turabturqb@gmail.com>
2025-06-18 17:20:19 +0300

    api

--- routes/jobApplicationRoutes.js
+++ routes/jobApplicationRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
-const Job = require("../models/jobModel");
+const Job = require("../models/JobModel");
 const JobApplication = require("../models/jobApplicationModel");
 const authMiddleware = require("../middleware/authMiddleware");
 
